eb8b9b90e7a92a0efb4bd3af7621eaf62f5594ff
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 09:00:03 +0700

    fix

---
+++ src/if1/pkg10119006/latihan9/IF110119006Latihan9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan9;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Text
+ */
+public class IF110119006Latihan9 {
+
+    public static void main(String[] args) {
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+       System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+    
+}
